a677535d6ba5483fbc840687d08ecf2bf1d751e2
Mohammed Subhan Ayaz Zama <66430769+subhanayaz@users.noreply.github.com>
2021-12-14 18:53:22 +0530

    Add files via upload

---
+++ Leap year/index.js
@@ -0,0 +1,23 @@
+var year = 2020;
+if(year % 4 ===0)
+{
+    if(year % 100 ===0)
+    {
+        if(year % 400 ===0)
+        {
+            console.log("The entered year " + year + " is a leap year");
+        }
+        else
+        {
+            console.log("The entered year " + year + " is not a leap year");
+        }
+    }
+    else
+    {
+        console.log("The entered year " + year + " is a leap year");
+    }
+}
+else
+{
+    console.log("The entered year " + year + " is not a leap year")
+}
